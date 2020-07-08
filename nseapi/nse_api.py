8783cf959f8c126bbc79cac6e71c9e70ae02c71f
rakhesh-r <rksh77@gmail.com>
2020-07-09 03:13:11 +0530

    add more methods and tests

--- nseapi/nse_api.py
+++ nseapi/nse_api.py
@@ -9,18 +9,35 @@ class NseApi:
         self._NSE_EQUITY_MASTER = "https://www.nseindia.com/api/equity-master"
 
         self._NSE_STOCK_DATA_INDEX_BASED = "https://www.nseindia.com/api/equity-stockIndices"
-        self.NSE_STOCK_DATA_INDEX_BASED_PARAMS = lambda index: {'index': index}
+        self._NSE_STOCK_DATA_INDEX_BASED_PARAMS = lambda index: {'index': index}
 
-    def get_top_gainers(self, index, top_n):
-        r = http_client.get(self._NSE_STOCK_DATA_INDEX_BASED, self.NSE_STOCK_DATA_INDEX_BASED_PARAMS(index))
+    def get_top_n_gainers(self, index, top_n):
+        dict_stocks = self._get_stocks_by_index(index)
+        dict_stocks.sort(key=lambda stock: (stock['pChange']), reverse=True)
+        dict_stocks = dict_stocks[:top_n]
+        return dict_stocks
+
+    def get_top_n_losers(self, index, top_n):
+        dict_stocks = self._get_stocks_by_index(index)
+        dict_stocks.sort(key=lambda stock: (stock['pChange']), reverse=False)
+        dict_stocks = dict_stocks[:top_n]
+        return dict_stocks
+
+    def get_top_gainers_above_perc(self, index, perc):
+        dict_stocks = self._get_stocks_by_index(index)
+        dict_stocks = [stock for stock in dict_stocks if stock['pChange'] >= perc]
+        return dict_stocks
+
+    def get_top_losers_below_perc(self, index, perc):
+        dict_stocks = self._get_stocks_by_index(index)
+        dict_stocks = [stock for stock in dict_stocks if stock['pChange'] <= perc]
+        return dict_stocks
+
+    def _get_stocks_by_index(self, index):
+        r = http_client.get(self._NSE_STOCK_DATA_INDEX_BASED, self._NSE_STOCK_DATA_INDEX_BASED_PARAMS(index))
         if not r.ok:
             raise Exception("Failed to call " + "https://www.nseindia.com/api/equity-stockIndices")
         dict_stocks = r.json()['data']
         # Exclude index from the list
         dict_stocks = [stock for stock in dict_stocks if stock['priority'] == 0]
-        dict_stocks.sort(key=lambda stock: (stock['pChange']), reverse=False)
-        dict_stocks = dict_stocks[:top_n]
-        return dict_stocks
-
-    def dummy_method(self, input_1: object) -> object:
-        return input_1
+        return  dict_stocks
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,5 +1,6 @@
 from .context import nseapi
 import unittest
+import random
 
 
 class BasicTestSuite(unittest.TestCase):
@@ -8,15 +9,38 @@ class BasicTestSuite(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         """setup"""
+        print(__name__)
         cls._nse_api = nseapi.NseApi()
 
-    def test_dummy_method(self):
-        input_1: bool = True
-        assert input_1 == self._nse_api.dummy_method(input_1)
-
-    def test_get_top_gainers(self):
-        output = self._nse_api.get_top_gainers('NIFTY 50', 5)
+    def test_get_top_n_gainers(self):
+        print("Test test_get_top_n_gainers")
+        output = self._nse_api.get_top_n_gainers('NIFTY 50', 5)
+        print(output)
         assert len(output) == 5
+
+    def test_get_top_n_losers(self):
+        print("Test test_get_top_n_losers")
+        output = self._nse_api.get_top_n_losers('NIFTY 50', 10)
+        print(output)
+        assert len(output) == 10
+
+    def test_get_top_gainers_above_perc(self):
+        print("Test test_get_top_gainers_above_perc")
+        output = self._nse_api.get_top_gainers_above_perc('NIFTY 50', 2.5)
+        print(output)
+        if len(output) > 0:
+            assert output[random.randint(0, len(output))]['pChange'] >= 2.5
+        else:
+            assert True
+
+    def test_get_top_losers_below_perc(self):
+        print("Test test_get_top_losers_below_perc")
+        output = self._nse_api.get_top_losers_below_perc('NIFTY 50', -4.62)
+        print(output)
+        if len(output) > 0:
+            assert output[random.randint(0, len(output))]['pChange'] <= -4.62
+        else:
+            assert True
 
 
 if __name__ == '__main__':
