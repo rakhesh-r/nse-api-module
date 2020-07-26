6a18f34a16ff798a273f81562c7882053273813e
rakhesh-r <rksh77@gmail.com>
2020-07-26 21:16:53 +0530

    fix a test case

--- nseapi/http_client.py
+++ nseapi/http_client.py
@@ -2,10 +2,10 @@ import requests
 
 
 def get(url, params):
-    return requests.get(url, headers=_get_headers(), params=params)
+    return requests.get(url, headers=__get_headers(), params=params)
 
 
-def _get_headers():
+def __get_headers():
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0',
                'Host': 'www.nseindia.com',
                'Accept-Encoding': 'gzip, deflate, br'}
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -30,7 +30,7 @@ class BasicTestSuite(unittest.TestCase):
         output = self._nse_api.get_top_gainers_above_perc('NIFTY 50', 2.5)
         print(output)
         if len(output) > 0:
-            assert output[random.randint(0, len(output))]['pChange'] >= 2.5
+            assert output[random.randint(0, len(output) - 1)]['pChange'] >= 2.5
         else:
             assert True
 
@@ -39,7 +39,7 @@ class BasicTestSuite(unittest.TestCase):
         output = self._nse_api.get_top_losers_below_perc('NIFTY 50', -4.62)
         print(output)
         if len(output) > 0:
-            assert output[random.randint(0, len(output))]['pChange'] <= -4.62
+            assert output[random.randint(0, len(output) - 1)]['pChange'] <= -4.62
         else:
             assert True
 
